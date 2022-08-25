05e0048aed22ef267bc61845b194ad9e519adf5e
Stefan Mitev <stefomitev@gmail.com>
2022-08-25 17:11:27 +0200

    run data hook service on interval

--- src/hooks/usePodData.ts
+++ src/hooks/usePodData.ts
@@ -1,15 +1,21 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { services } from "../services";
+import { config } from "../config/env";
 
 export const usePodData = (filters?: Filter<Pod>[], sort?: Sort<Pod>) => {
   const [pods, setPods] = useState<Pod[]>([]);
 
-  // set timeout
-  useEffect(() => {
+  const getPods = useCallback(() => {
     services.kubeService.getPods(filters, sort).then((pods) => {
       setPods(pods);
     });
   }, [filters, sort]);
+
+  useEffect(() => {
+    getPods();
+    const interval = setInterval(getPods, 1000 * config.refreshInterval);
+    return () => clearInterval(interval);
+  }, [getPods]);
 
   return { pods };
 };
